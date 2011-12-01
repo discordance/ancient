0daa8ac238268c8299f94ede459e5dd252fa0095
Aymeric Nunge <aymeric.nunge@mediatech.fr>
2011-12-01 11:48:43 +0100

    Update: toward JSLint compliance

--- global/generator.js
+++ global/generator.js
@@ -14,22 +14,22 @@ var Util = require("./utilitor");
  * No special algorithm used here except Math.random();
  */
 exports.phGenerateUniform = function (size, density) {
-  var i = 0,
-  res = [],
-  score = 0,
-  step, vel = 0;
-  for (i; i < size; i++) {
-    score = Math.random();
-    if (score < density) {
-      vel = Math.floor(Util.normalRand(10, 4)); // using normal function, better results that a plain random
-      step = new Step(vel, 1, [], false, 0);
-    } else {
-      vel = 0;
-      step = new Step(vel, 1, [], false, 0); // silent step
+    var i = 0,
+    res = [],
+    score = 0,
+    step, vel = 0;
+    for (i; i < size; i++) {
+        score = Math.random();
+        if (score < density) {
+            vel = Math.floor(Util.normalRand(10, 4)); // using normal function, better results that a plain random
+            step = new Step(vel, 1, [], false, 0);
+        } else {
+            vel = 0;
+            step = new Step(vel, 1, [], false, 0); // silent step
+        }
+        res.push(step);
     }
-    res.push(step);
-  }
-  return new Phrase(size, res);
+    return new Phrase(size, res);
 };
 
 /**
@@ -37,30 +37,30 @@ exports.phGenerateUniform = function (size, density) {
  * this modulo parameter defines the beat division where a more important probability should to be applied.
  */
 exports.phGenerateRecurringSimple = function (size, modulo) {
-  var i = 0,
-  res = [],
-  prob = 0,
-  score = 0,
-  step, vel, dist = 0,
-  abs = Math.abs;
-  for (i; i < size; i++) {
-    dist = abs(i % 4 - modulo);
+    var i = 0,
+    res = [],
+    prob = 0,
+    score = 0,
+    step, vel, dist = 0,
+    abs = Math.abs;
+    for (i; i < size; i++) {
+        dist = abs(i % 4 - modulo);
     
-    if(dist){
-      prob = 1/(dist+1);
-    } else {
-      prob=0.98; // almost impossible to miss here, but life is a bitch sometimes
+        if(dist){
+            prob = 1/(dist+1);
+        } else {
+            prob=0.98; // almost impossible to miss here, but life is a bitch sometimes
+        }
+        score=Math.random(); // pure random here
+        if(score<prob){
+            vel = Math.floor(Util.normalRand(14*prob, 1)); // using normal function, in relation to the prob
+            step = new Step(vel, 1, [], false, 0);
+        }else{
+            vel = 0;
+            step = new Step(vel, 1, [], false, 0);
+        }
+        res.push(step);
     }
-    score=Math.random(); // pure random here
-    if(score<prob){
-      vel = Math.floor(Util.normalRand(14*prob, 1)); // using normal function, in relation to the prob
-      step = new Step(vel, 1, [], false, 0);
-    }else{
-      vel = 0;
-      step = new Step(vel, 1, [], false, 0);
-    }
-    res.push(step);
-  }
-  // returns and filter result below 5
-  return new Phrase(size, res).filter(5);
+    // returns and filter result below 5
+    return new Phrase(size, res).filter(5);
 };
--- global/utilitor.js
+++ global/utilitor.js
@@ -7,14 +7,14 @@
  * Add leading zeros (useful for dumping phrases)
  */
 exports.leadZero = function (n, totalDigits) {
-  n = n.toString();
-  var pd = '', i=0;
-  if (totalDigits > n.length) {
-    for (i; i < (totalDigits - n.length); i++) {
-      pd += '0';
+    n = n.toString();
+    var pd = '', i=0;
+    if (totalDigits > n.length) {
+        for (i; i < (totalDigits - n.length); i++) {
+            pd += '0';
+        }
     }
-  }
-  return pd + n.toString();
+    return pd + n.toString();
 };
 
 /**
@@ -22,36 +22,36 @@ exports.leadZero = function (n, totalDigits) {
  */ 
 exports.map = function (value, min1, max1, min2, max2)
 {
-  var norm = function(val, min, max){
-    return (val - min) / (max - min);
-  }
-  var interp = function(val, min, max){
-    return min + (max - min) * val;
-  }
-  return interp( norm(value, min1, max1), min2, max2);
+    var norm = function(val, min, max){
+        return (val - min) / (max - min);
+    }
+    var interp = function(val, min, max){
+        return min + (max - min) * val;
+    }
+    return interp( norm(value, min1, max1), min2, max2);
 }
 
 /**
  * Takes two nibbles and concat to one byte
  */
 exports.nib2byte = function (a, b) {
-  return (a << 4) | b;
+    return (a << 4) | b;
 };
 
 /**
  * Takes one byte and extract two nibbles in an array
  */
 exports.byte2nib = function (by) {
-  var res = [];
-  res[0] = (by >> 4) & 0x0F;
-  res[1] = (by & 0x0F);
-  return res;
+    var res = [];
+    res[0] = (by >> 4) & 0x0F;
+    res[1] = (by & 0x0F);
+    return res;
 };
 
 /**
  * Generate a fast normal distributed random number based on mean and standard deviation passed in parametters.
  */ 
 exports.normalRand = function(mean, stdev) {
-  var rnd = (Math.random()*2-1)+(Math.random()*2-1)+(Math.random()*2-1);
-  return rnd*stdev+mean;
+    var rnd = (Math.random()*2-1)+(Math.random()*2-1)+(Math.random()*2-1);
+    return rnd*stdev+mean;
 };
