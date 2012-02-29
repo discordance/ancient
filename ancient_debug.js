9a29253e09517e1d9347994b9e7cdf2900a7e9dc
Aymeric Nunge <aymeric.nunge@mediatech.fr>
2012-02-29 17:41:14 +0100

    Live Coding for sequencer soooon sooon

--- ancient_debug.js
+++ ancient_debug.js
@@ -10,17 +10,19 @@ var Gen = require("./global/generator");
 var Ext =  require("./external/translator");
 var RtSeq = require("./midi/rtseq");
 
-/*
+
 var Fs = require('fs');
 var a = new Phrase(16, []);
 a.setStrVel("f000f000f000f000");
 
 var b = new Phrase(16, []);
 b.setStrVel("00000000ff000000");
-
+/*
 var c = Gen.phLinearMorph(a, b, 0.2);
 console.log(c.dumpVel());
 */
 
 var seq = new RtSeq();
+//seq.start();
+seq.phParse([a,b]);
 seq.start();
--- midi/rtseq.js
+++ midi/rtseq.js
@@ -1,4 +1,6 @@
 var midi = require('midi');
+var Phrase = require("../data_structure/phrase");
+var Util = require("../global/utilitor");
 
 function RtSeq() {
 	
@@ -7,6 +9,9 @@ function RtSeq() {
 	this.ticks = 0;
 	this.bpm = 120;
 	this.events = new Array(this.max);
+	for (var i = this.max - 1; i >= 0; i--) {
+		this.events[i] = [];
+	};
 	this.dly = (60000/this.bpm)/96;
 	this.rtmidi = null;
 
@@ -20,8 +25,29 @@ RtSeq.prototype = {
 	* Test a raw array of phrases
 	* Final version would be Tracks ?	
 	*/
-	parsePhrases: function(phrases) {
-		
+	phParse: function(phrases) {
+		var i=0, j=0, l=phrases.length, cp, pl=0, st, subi=0, ct=0, cv=0;
+		var map = Util.map;
+		var pmap = [36, 38, 40, 42, 46, 47, 48, 50] // for testing pitchs
+		for (var i = 0; i < l; i++) {
+			cp = phrases[i];
+			pl=cp.size;
+			for (var j = 0; j < 128; j++) {
+				subi = j%pl;
+				st=cp.steps[subi];
+				if(st.vel){
+					ct = map(j, 0, 128, 0, this.max);
+					cv=map(st.vel,0,15,0,127);
+					if(ct+1 <= this.max-1){
+						this.events[ct].push({s:153,p:pmap[i],v:cv})
+						this.events[ct+1].push({s:137,p:pmap[i],v:cv})
+					}
+				}
+				//console.log(st.drift);
+			};
+		};
+
+		//console.log(this.events);
 	},
 	start: function() {
 		// Open the first available output port.
@@ -30,6 +56,14 @@ RtSeq.prototype = {
 		this.ivid = setInterval(function() { oThis.tick() }, this.dly);
 	},
 	tick: function() {
+		var l = this.events[this.ticks].length, ob;
+		if(l){
+			for (var i = l - 1; i >= 0; i--) {
+				ob=this.events[this.ticks][i];
+				this.out.sendMessage([ob.s,ob.p,ob.v]);
+			};
+		}
+
 		this.ticks++;
 		this.ticks = this.ticks % this.max;
 		//console.log(this.ticks);
